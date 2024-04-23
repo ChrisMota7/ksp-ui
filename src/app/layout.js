78860d2b19c5c4150ea73a05a23c4270d6be90cc
ChrisMota7 <19240532@leon.tecnm.mx>
2024-04-22 20:49:30 -0600

    Menu added

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,9 +1,11 @@
 "use client"
 import { Inter } from "next/font/google";
 
-import { Provider } from 'react-redux';
+import { Provider, useSelector } from 'react-redux';
 import { configureStore } from "@reduxjs/toolkit";
 import reducer from "@/redux/reducer"
+import Menu from "@/components/Menu/Menu";
+import { selectUserid } from "@/redux/reducers/authReducer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,12 +14,19 @@ const store = configureStore({
 })
 
 export default function RootLayout({ children }) {
+  // const isUserId = useSelector(selectUserid)
+  // const isUserId = 12
+  const isUserId = undefined
+
   return (
     <html lang="en">
       <Provider store={store}>
         <body className={inter.className}>
-          {/* <Menu /> */}
-          {children}
+          {isUserId ? (
+            <Menu children={children} />
+          ) : (
+            children
+          )}
         </body>
       </Provider>
     </html>
---
+++ src/components/Menu/Menu.jsx
@@ -0,0 +1,205 @@
+import React, { useState } from 'react';
+import { useRouter } from "next/navigation";
+
+import { styled, useTheme } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import MuiDrawer from '@mui/material/Drawer';
+import MuiAppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import List from '@mui/material/List';
+import CssBaseline from '@mui/material/CssBaseline';
+import Divider from '@mui/material/Divider';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+
+import ListAltIcon from '@mui/icons-material/ListAlt';
+import PlaylistAddIcon from '@mui/icons-material/PlaylistAdd';
+import DnsIcon from '@mui/icons-material/Dns';
+import EqualizerIcon from '@mui/icons-material/Equalizer';
+import SettingsIcon from '@mui/icons-material/Settings';
+
+const drawerWidth = 240;
+
+const openedMixin = (theme) => ({
+  width: drawerWidth,
+  transition: theme.transitions.create('width', {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.enteringScreen,
+  }),
+  overflowX: 'hidden',
+});
+
+const closedMixin = (theme) => ({
+  transition: theme.transitions.create('width', {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  overflowX: 'hidden',
+  width: `calc(${theme.spacing(7)} + 1px)`,
+  [theme.breakpoints.up('sm')]: {
+    width: `calc(${theme.spacing(8)} + 1px)`,
+  },
+});
+
+const DrawerHeader = styled('div')(({ theme }) => ({
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'flex-end',
+  padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
+}));
+
+const AppBar = styled(MuiAppBar, {
+  shouldForwardProp: (prop) => prop !== 'open',
+})(({ theme, open }) => ({
+  zIndex: theme.zIndex.drawer + 1,
+  transition: theme.transitions.create(['width', 'margin'], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  ...(open && {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  }),
+}));
+
+const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' })(
+  ({ theme, open }) => ({
+    width: drawerWidth,
+    flexShrink: 0,
+    whiteSpace: 'nowrap',
+    boxSizing: 'border-box',
+    ...(open && {
+      ...openedMixin(theme),
+      '& .MuiDrawer-paper': openedMixin(theme),
+    }),
+    ...(!open && {
+      ...closedMixin(theme),
+      '& .MuiDrawer-paper': closedMixin(theme),
+    }),
+  }),
+);
+
+const Menu = ({ children }) => {
+  const { push } = useRouter()
+  const menuItems = ["Mis Tickets", "Crear Ticket", "Categorías", "Dashboard", "Configuración"]
+  const theme = useTheme();
+  const [open, setOpen] = useState(false);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
+  const renderItemIcon = (itemText) => {
+    switch (itemText) {
+      case "Mis Tickets":
+        return (
+          <ListAltIcon />
+        )
+      case "Crear Ticket":
+        return (
+          <PlaylistAddIcon />
+        )
+      case "Categorías":
+        return (
+          <DnsIcon />
+        )
+      case "Dashboard":
+        return (
+          <EqualizerIcon />
+        )
+      case "Configuración":
+        return (
+          <SettingsIcon />
+        )
+    }
+  }
+
+  const handleItemClick = (itemText)=> {
+    switch (itemText) {
+      case "Mis Tickets":
+        push("/tickets")
+      case "Crear Ticket":
+        push("/tickets")
+      case "Categorías":
+        push("/tickets")
+      case "Dashboard":
+        push("/tickets")
+      case "Configuración":
+        push("/tickets")
+    }
+  }
+
+  return (
+    <Box sx={{ display: 'flex' }}>
+      <CssBaseline />
+      <AppBar position="fixed" open={open}>
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            edge="start"
+            sx={{
+              marginRight: 5,
+              ...(open && { display: 'none' }),
+            }}
+          >
+            <MenuIcon />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+      <Drawer variant="permanent" open={open}>
+        <DrawerHeader sx={{
+            display: "flex",
+            justifyContent: "space-between",
+            paddingLeft: 2
+        }}>
+            <img src="/LogoNegro.png" alt="KSP" height={30} />
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon />}
+          </IconButton>
+        </DrawerHeader>
+        <Divider />
+        <List>
+          {menuItems.map((itemText) => {
+            return (
+              <div key={itemText}>
+              <ListItem disablePadding sx={{ display: 'block' }}>
+                <ListItemButton onClick={() => handleItemClick(itemText)} className={open ? "menu__open-list-item" : "menu__list-item"}>
+                  <ListItemIcon className={open ? "menu__open-list-item__list-icon" : "menu__list-item__list-icon"}>
+                    {renderItemIcon(itemText)}
+                  </ListItemIcon>
+                  <ListItemText primary={itemText} sx={{ opacity: open ? 1 : 0 }} />
+                </ListItemButton>
+              </ListItem>
+              <Divider />
+              </div>
+            )
+          })}
+        </List>
+      </Drawer> 
+      <Box component="main" sx={{ flexGrow: 1, paddingLeft: 10 }}>
+        <DrawerHeader />
+        {children}
+      </Box>
+    </Box>
+  );
+}
+
+export default Menu
