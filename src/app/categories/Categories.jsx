1cd431296d0b5925d18ecfc86edfa8df504b56d5
ChrisMota7 <19240532@leon.tecnm.mx>
2024-04-24 00:59:12 -0600

    Categories ui enhaced

--- src/app/categories/Categories.jsx
+++ src/app/categories/Categories.jsx
@@ -6,7 +6,7 @@ import { useRouter } from 'next/navigation';
 import { useDispatch, useSelector } from 'react-redux';
 import { getCategories, getProblems } from '@/redux/actions/categoryActions';
 import { selectCategories } from '@/redux/reducers/categoryReducer';
-import { Card, CardContent, Breadcrumbs, Link, Typography, Button } from '@mui/material';
+import { Breadcrumbs, Link, Typography, Button } from '@mui/material';
 import { Table, TableBody, TableCell, TableHead, TableRow, Paper, IconButton, Tooltip } from '@mui/material';
 import VisibilityIcon from '@mui/icons-material/Visibility';
 import DeleteIcon from '@mui/icons-material/Delete';
@@ -21,64 +21,58 @@ const Categories = () => {
     }, [dispatch]);
     
     return(
-        <div className='header'>
-        <div className='header__title'>
+      <div className='categories'>
+        <div className='categories__header__title'>
           <h1>Categorías</h1>
         </div>
-        <div className='header__nav'>
+
+        <div className='categories__header__nav'>
           <Breadcrumbs aria-label="breadcrumb">
             <Link underline="hover" color="inherit" >
               HelpDesk
             </Link>
-            <Link underline="hover" color="inherit">
-              Tickets
-            </Link>
-            <Typography color="text.primary">Solicitudes</Typography>
+            <Typography color="text.primary">Categorías</Typography>
           </Breadcrumbs>
         </div>
-        <div className='content'>
-          <Card>
-            <div className='content__divisor'>
-            <CardContent className='content__table'>
-              <div className='content__button'>
-                <Button variant="contained" onClick={() => router.push('/New-Category')}>Nueva categoría</Button>
-              </div>
-              <div>
-                <Paper>
-                    <Table>
-                        <TableHead className='table'>
-                            <TableRow>
-                                <TableCell className='table__letra'>ID</TableCell>
-                                <TableCell className='table__letra'>Nombre</TableCell>
-                                <TableCell className='table__letra'>Acciones</TableCell>
+
+        <div className='categories__info'>
+          <div className='categories__info__new-category'>
+            <Button variant="contained" onClick={() => router.push('/New-Category')}>Nueva categoría</Button>
+          </div>
+
+          <div>
+            <Paper>
+                <Table>
+                    <TableHead className='categories__info__table'>
+                        <TableRow>
+                            <TableCell className='categories__info__table__headers'>ID</TableCell>
+                            <TableCell className='categories__info__table__headers'>Nombre</TableCell>
+                            <TableCell className='categories__info__table__headers'>Acciones</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {categories.map(category => (
+                            <TableRow key={category.id}>
+                                <TableCell>{category.id}</TableCell>
+                                <TableCell>{category.name}</TableCell>
+                                <TableCell>
+                                    <Tooltip title="Ver Detalles">
+                                        <IconButton onClick={() => router.push(`/tickets/${ticket.id}`)}>
+                                            <VisibilityIcon />
+                                        </IconButton>
+                                    </Tooltip>
+                                    <Tooltip title="Eliminar Ticket">
+                                        <IconButton onClick={() => deleteTicket(ticket.id)}>
+                                            <DeleteIcon />
+                                        </IconButton>
+                                    </Tooltip>
+                                </TableCell>
                             </TableRow>
-                        </TableHead>
-                        <TableBody>
-                            {categories.map(category => (
-                                <TableRow key={category.id}>
-                                    <TableCell>{category.id}</TableCell>
-                                    <TableCell>{category.name}</TableCell>
-                                    <TableCell>
-                                        <Tooltip title="Ver Detalles">
-                                            <IconButton onClick={() => router.push(`/tickets/${ticket.id}`)}>
-                                                <VisibilityIcon />
-                                            </IconButton>
-                                        </Tooltip>
-                                        <Tooltip title="Eliminar Ticket">
-                                            <IconButton onClick={() => deleteTicket(ticket.id)}>
-                                                <DeleteIcon />
-                                            </IconButton>
-                                        </Tooltip>
-                                    </TableCell>
-                                </TableRow>
-                            ))}    
-                        </TableBody>
-                    </Table>
-                </Paper>
-              </div>
-            </CardContent>
-            </div>
-          </Card>
+                        ))}    
+                    </TableBody>
+                </Table>
+            </Paper>
+          </div>
         </div>
       </div>
     )
